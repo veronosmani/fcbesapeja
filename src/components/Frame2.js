9fefc9a782bb8e7ae11f0ea33da94c7a568a9f84
veronosmani <veron.osmani@gmail.com>
2024-06-11 15:34:05 +0200

    new file:   public/picture2.png 	modified:   public/picture3.png 	new file:   public/picture4.png 	new file:   public/picture5.png 	modified:   src/components/Frame2.js 	modified:   src/index.css

--- src/components/Frame2.js
+++ src/components/Frame2.js
@@ -10,7 +10,35 @@ export default function Frame2() {
       <p className="Frame2Text1">VIDEO FC BESA PEJA</p>
 
       <h1 className="Frame2Text2">KOMUNIKATA & LAJME</h1>
-      <img className="Frame2Picture2" src="picture3.png" />
+      <img className="Frame2Picture2" src="picture2.png" />
+
+      <img className="Frame2Picture3" src="picture3.png" />
+      <div className="Picture3Box">
+        <h1 className="Picture3Title">Lorem Ipsum</h1>
+        <p className="Picture3Text">
+          It has survived not only five centuries, but also the leap into
+          electronic...
+        </p>
+      </div>
+
+      <img className="Frame2Picture4" src="picture4.png" />
+      <div className="Picture4Box">
+        <h1 className="Picture4Title">Lorem Ipsum</h1>
+        <p className="Picture4Text">
+          It has survived not only five centuries, but also the leap into
+          electronic...
+        </p>
+      </div>
+      <img className="Frame2Picture5" src="picture5.png" />
+      <div className="Picture5Box">
+        <h1 className="Picture5Title">Lorem Ipsum</h1>
+        <p className="Picture5Text">
+          It has survived not only five centuries, but also the leap into
+          electronic...
+        </p>
+      </div>
+
+      <button className="Frame2SeeMore">See more   <i class="fa-solid fa-arrow-right-long"></i></button>
     </div>
   );
 }
