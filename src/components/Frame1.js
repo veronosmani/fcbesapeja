1cae1c4b1a748b52bf03e60926d941229b61eb2d
veronosmani <veron.osmani@gmail.com>
2024-06-11 14:14:14 +0200

    new file:   public/hero-base.jpg 	new file:   public/hero-base.png 	new file:   src/components/Frame1.js 	modified:   src/index.css 	modified:   src/index.js

---
+++ src/components/Frame1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Frame1() {
+  return (
+    <div className='Frame1'>
+        <img className='hero-base' src='hero-base.png' />
+        <h2 className='Frame1Text1'>Historiku për formimin e klubit </h2>
+        <h1 className='Frame1Text2'>FC Besa Peja</h1>
+        <button className='Frame1Button1'>Historiku i klubit</button>
+        
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Header from './components/header';
+import Frame1 from './components/Frame1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Header />
+    <Frame1 />
   </React.StrictMode>
 )
