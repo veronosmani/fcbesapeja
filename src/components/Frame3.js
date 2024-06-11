64004f816a2b0bfa958e4c4c21a4aaa9097d328f
veronosmani <veron.osmani@gmail.com>
2024-06-11 16:01:04 +0200

    new file:   public/picture6.png 	new file:   src/components/Frame3.js 	modified:   src/index.css 	modified:   src/index.js

---
+++ src/components/Frame3.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Frame3() {
+  return (
+    <div className="Frame3">
+      <img className="Frame3Picture6" src="picture6.png" />
+
+      <h1 className="NumberOfMatches">225+</h1>
+      <h2 className="Matches">TOTAL MATCHES</h2>
+
+      <h1 className="NumberOfTrainers">5+</h1>
+      <h2 className="Trainers">TOTAL TRAINERS</h2>
+
+      <h1 className="NumberOfPlayers">38+</h1>
+      <h2 className="Players">TOTAL PLAYERS</h2>
+
+      <h1 className="NumberOfAwards">12+</h1>
+      <h2 className="Awards">TOTAL AWARDS</h2>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import Header from './components/header';
 import Frame1 from './components/Frame1';
 import Frame2 from './components/Frame2';
-
+import Frame3 from './components/Frame3';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -13,5 +13,6 @@ root.render(
     <Header />
     <Frame1 />
     <Frame2 />
+    <Frame3 />
   </React.StrictMode>
 )
